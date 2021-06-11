ba1b2a2696d4007f5beb06dc55cfdfbcb0b694e9
Pro (@Pro_xD)❤️ <81475460+hKesko@users.noreply.github.com>
2021-06-11 16:02:05 +0800

    Update android.py

--- utah/modules/android.py
+++ utah/modules/android.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import rapidjson
+import rapidjson as json
 from requests import get
 from yaml import load, Loader
 from bs4 import BeautifulSoup
